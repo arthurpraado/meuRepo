7c8c0bfb5ebc9d0e13c36f65aa50a5161d3bc126
Rodrigo Orsi Pagotto <arthurbmprado@gmail.com>
2023-08-31 14:57:51 -0300

    variaveis

---
+++ template_javascript/index.js
@@ -0,0 +1,49 @@
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+
+// console.log(a, b)
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+// console.log(a, b, c)
+
+// let horasTrabalhadas = prompt ("Quantas horas você trabalha por dia?")
+// let recebePorDia = prompt ("Quanto você recebe por dia?")
+// alert (`Você recebe $${recebePorDia/horasTrabalhadas} por hora`)
+
+// let nome = prompt (`Qual seu nome?`)
+// let idade = +prompt (`Qual sua idade?`)
+
+// console.log(typeof nome, typeof idade)
+
+// console.log(`Olá ${nome}, você tem ${idade} anos.`)
+
+// const palmeiras = prompt (`Tem Palmeiras hoje?`)
+// const sol = prompt (`Está sol hoje?`)
+// const estudando = prompt (`Você está estudando hoje?`)
+
+// console.log(`Tem Palmeiras hoje? ${palmeiras.toUpperCase()}.`)
+// console.log( `Está sol hoje? ${sol.toUpperCase()}.`)
+// console.log(`Você está estudando hoje? ${estudando.toUpperCase()}.`)
+
+let a = 10
+let b = 25
+let c 
+c = b
+b = a
+a = c
+console.log("O novo valor de A é:", a, "O novo valor de B é:", b)
+
+let numeroUm = +prompt ("Insira um número.")
+let numeroDois = +prompt ("Insira outro número.")
+
+console.log(`O primeiro número somado ao segundo número resulta em: ${numeroUm + numeroDois}`)
+console.log(`O primeiro número multiplicado pelo segundo número resulta em: ${numeroUm * numeroDois}`)
+
