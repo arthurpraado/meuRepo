cf8f8f971a9f91966c25f3a5bcb35ee66e7888d6
Rodrigo Orsi Pagotto <arthurbmprado@gmail.com>
2023-09-11 13:50:02 -0300

    projeto blackjack com desafio

--- projeto-blackjack/desafio.js
+++ projeto-blackjack/desafio.js
@@ -1,60 +1,76 @@
-    let cartasUsuario = []
-    let cartasComputador = []
-    let cartasSomadas = 0
+    let cartasUsuario = [comprarCarta(), comprarCarta()]
+    let cartasComputador = [comprarCarta(), comprarCarta()]
+    let comecaJogo = false
 
-   
+   const mostrarCartas = (arrayDeCartas) => {
+       return arrayDeCartas.reduce((prev, next) => {
+         return prev += next.texto+" "
+      }, "")
+   }
 
-   //  const pontuacaoUsuario = carta.valor 
-   //  const pontuacaoComputador = cartaComputador.valor 
-         
-    
-    if(confirm("Boas vindas ao jogo de Blackjack! Quer iniciar uma nova rodada?")) {
-      cartasUsuario.push(comprarCarta())
-      cartasUsuario.push(comprarCarta())
-      cartasComputador.push(comprarCarta())
-      } 
+   const somarPontos = (arrayDeCartas) => {
+       return arrayDeCartas.reduce((prev, next) => {
+         return prev += next.valor
+      }, 0)
+   }
 
-      else if (cartasUsuario.filter((carta) => {return carta.texto.numero && carta.texto.numero === "A"})) {
-         cartasUsuario.push(comprarCarta())
-         cartasUsuario.push(comprarCarta())
-      } 
-
-      else if (cartasComputador.filter((carta) => {return carta.texto.numero && carta.texto.numero === "A"})){
-         cartasComputador.push(comprarCarta())
-         } 
-        
-       for (let i = 0; i < cartasUsuario.length; i++) {
-            cartasSomadas += cartasUsuario[i]
-          }
-         
-      if(confirm(`Suas cartas são ${[cartasUsuario.map((carta) => {return carta.texto})]}. A carta revelada do computador é ${cartasComputador.map((carta) => {return carta.texto})}. Deseja comprar mais uma carta?`)) {
-         cartasUsuario.push(comprarCarta())
-      } else {
-         
-         (cartasUsuario.map(() => {return cartasSomadas > 21}) ) 
-         alert(`Usuário - Cartas: ${cartasUsuario.texto} - Pontuação: ${cartasUsuario.valor} 
-         Computador - Cartas: ${cartasComputador.texto} - Pontuação: ${cartasComputador.valor}
-         O Computador ganhou!`)
+   const eParDeAs = () => {
+      if (somarPontos(cartasUsuario) === 22 || somarPontos(cartasComputador) === 22) {
+         cartasUsuario = [comprarCarta(), comprarCarta()]
+         cartasComputador = [comprarCarta(), comprarCarta()]
       }
-      
-      if(confirm(`Suas cartas são ${[cartasUsuario.map((carta) => {return carta.texto})]}. A carta revelada do computador é ${cartasComputador.map((carta) => {return carta.texto})}. Deseja comprar mais uma carta?`)) {
+   }
+
+   const retornaPontuacao = (usuario) => {
+      let quemGanhou = "O computador ganhou"
+      if (usuario) {
+         quemGanhou = "O usuário ganhou"
+      }
+      return alert(`
+            Usuário - Cartas: ${mostrarCartas(cartasUsuario)} - Pontuação: ${somarPontos(cartasUsuario)} 
+            Computador - Cartas: ${mostrarCartas(cartasComputador)} - Pontuação: ${somarPontos(cartasComputador)}
+            ${quemGanhou}!`)
+   }
+   
+    if(confirm("Boas vindas ao jogo de Blackjack! Quer iniciar uma nova rodada?")) {
+      comecaJogo = true
+      eParDeAs()
+   } else {
+      alert("O jogo acabou.")
+   }
+   while (comecaJogo) {
+      if (confirm(`  
+      Suas cartas são: ${mostrarCartas(cartasUsuario)}
+      A carta revelada do computador é ${cartasComputador[0].texto}. 
+      Deseja comprar mais uma carta?`)) {
          cartasUsuario.push(comprarCarta())
-         }  else {
-             (cartasUsuario.map((carta) => {return carta.valor > 21}) ) 
-               alert(`Usuário - Cartas: ${cartasUsuario.texto} - Pontuação: ${cartasUsuario.valor} 
-               Computador - Cartas: ${cartasComputador.texto} - Pontuação: ${cartasComputador.valor}
-               O Computador ganhou!`)
+         if (somarPontos(cartasUsuario) > 21 ) {
+            comecaJogo = false
+            retornaPontuacao(false)
          }
-         
+      } else {
+         comecaJogo = false
+         if (somarPontos(cartasComputador) < somarPontos(cartasUsuario)) {
+            retornaPontuacao(true)
+         } else {
+            retornaPontuacao(false)
+         }
+      }
+   }
+
+
+      
+    
+     
+      
+
+     
+   
+   
             
       
           
       
-         
-         
-      // if (a){
-      //    (confirm(`Suas cartas são ${[cartasUsuario]}. A carta revelada do computador é ${cartaComputador.texto}. Deseja comprar mais uma carta?`))
-      // }
       
      
     
@@ -68,14 +84,3 @@
 
 
 
-
-
-   //     if (pontuacaoUsuario > pontuacaoComputador) {
-   //        console.log("O usuário ganhou!")
-   //     } else if (pontuacaoUsuario < pontuacaoComputador) {
-   //        console.log("O computador ganhou ):")
-   //     } else if (pontuacaoComputador === pontuacaoUsuario) {
-   //        console.log("Empate!")
-   //     }
-    
-    
